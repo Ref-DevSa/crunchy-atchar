1965e6cafe5f1118329310dd84b9618bcd3ca0ae
Ref-DevSa <ref.devsa@gmail.com>
2026-07-19 04:25:53 +0200

    Updated homepage featured products section

---
+++ src/data/products.js
@@ -0,0 +1,22 @@
+const products = [
+  {
+    id: 1,
+    name: "Mild Mango Atchar",
+    price: "R55",
+    image: "https://images.unsplash.com/photo-1504674900247-0877df9cc836?w=500",
+  },
+  {
+    id: 2,
+    name: "Hot Mango Atchar",
+    price: "R60",
+    image: "https://images.unsplash.com/photo-1546069901-ba9599a7e63c?w=500",
+  },
+  {
+    id: 3,
+    name: "Mixed Vegetable Atchar",
+    price: "R65",
+    image: "https://images.unsplash.com/photo-1512621776951-a57141f2eefd?w=500",
+  },
+];
+
+export default products;
