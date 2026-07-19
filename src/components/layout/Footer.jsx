d016ab9fb6f1a74c64e5014f8038be385023958d
Ref-DevSa <ref.devsa@gmail.com>
2026-07-19 03:46:59 +0200

    Updated Footer

---
+++ src/components/layout/Footer.jsx
@@ -0,0 +1,11 @@
+function Footer() {
+  return (
+    <footer className="bg-green-900 py-8 text-center text-white">
+      <p>
+        © {new Date().getFullYear()} Crunchy Atchar. All Rights Reserved.
+      </p>
+    </footer>
+  );
+}
+
+export default Footer;
