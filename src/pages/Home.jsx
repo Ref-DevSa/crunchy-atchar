07cba564fe986fcf1fe9329498369ec006f30a70
Ref-DevSa <ref.devsa@gmail.com>
2026-07-19 04:35:59 +0200

    Home page updated.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import Hero from "../components/home/Hero";
 import FeaturedProducts from "../components/home/FeaturedProducts";
+import AboutPreview from "../components/home/AboutPreview";
 
 function Home() {
   return (
     <>
       <Hero />
       <FeaturedProducts />
+      <AboutPreview />
     </>
   );
 }
